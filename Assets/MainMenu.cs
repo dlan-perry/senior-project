811fe8b57125b2ca10e6bb01be30f18196edb3d2
curtin53 <curtin53@students.rowan.edu>
2023-02-19 19:20:07 -0500

    menu
    adding buttons and menu for launching game

---
+++ Assets/MainMenu.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    void Start()
+    {
+        Time.timeScale = 1;
+        //SceneManager.LoadScene("MainMenu", LoadSceneMode.Additive); //don't do this
+    }
+    public void PlayGame()
+    {
+        SceneManager.LoadScene("Game", LoadSceneMode.Single); //Loads Game scene
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit(); //quit button
+    }
+
+    public void MainMenutoLeaderboard()
+    {
+        SceneManager.LoadScene("Leaderboard", LoadSceneMode.Single); //single was giving problems, trying additive
+    }
+
+    public void LeaderboardtoMainMenu()
+    {
+        SceneManager.LoadScene("MainMenu", LoadSceneMode.Single); //also trying additive
+    }
+}
