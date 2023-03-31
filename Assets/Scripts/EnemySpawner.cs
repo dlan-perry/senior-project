39f13ca43293e1dfd80166d7026a81601cb55109
chenzayden <zayden93@students.rowan.edu>
2023-03-30 21:53:21 -0400

    Cleaned up EnemySpawner

--- Assets/Scripts/EnemySpawner.cs
+++ Assets/Scripts/EnemySpawner.cs
@@ -21,46 +21,14 @@ public class EnemySpawner : MonoBehaviour
 
     private void SpawnNewEnemy()
     {
-        
-        //randomSpawnZone = Random.Range(0,4);
-
         randomXposition = Random.Range((float)(-8.5), (float)(7.5));
         randomYposition = Random.Range((float)(-3.5), (float)(9));
-
         
         spawnPosition = new Vector3(randomXposition, randomYposition, 0f);
         newEnemy = Instantiate(enemy, spawnPosition, Quaternion.identity);
         rend = newEnemy.GetComponent<SpriteRenderer>();
         rend.color = new Color(Random.Range(0,2), Random.Range(0,2), Random.Range(0,2), 1f);
 
-        
-        /*
-        switch(randomSpawnZone)
-        {
-            case 0:
-                randomXposition = Random.Range(-11f, -10f);
-                randomYposition = Random.Range(-8f, -8f);
-                break;
-            case 1:
-                randomXposition = Random.Range(-10f, 10f);
-                randomYposition = Random.Range(-7f, -8f);
-                break;
-            case 2:
-                randomXposition = Random.Range(-10f, -11f);
-                randomYposition = Random.Range(-8f, 8f);
-                break;
-            case 3:
-                randomXposition = Random.Range(-10f, 10f);
-                randomYposition = Random.Range(7f, 8f);
-                break;
-        }
-        
-
-        spawnPosition = new Vector3(randomXposition, randomYposition, 0f);
-        newEnemy = Instantiate(enemy, spawnPosition, Quaternion.identity);
-        rend = newEnemy.GetComponent<SpriteRenderer>();
-        rend.color = new Color(Random.Range(0,2), Random.Range(0,2), Random.Range(0,2), 1f);
-    */
     }
     // Update is called once per frame
     void Update()
